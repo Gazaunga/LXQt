e49eacebcb7fc203f3265e41fed120c03f530075
Gazaunga <Gazaunga@gmail.com>
2017-07-21 15:19:44 -0400

    Update fontstrap.sh

--- .local/share/fonts/fontstrap.sh
+++ .local/share/fonts/fontstrap.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# In download.sh
+# FiraCode
 for type in Bold Light Medium Regular Retina; do
     wget -O ~/.local/share/fonts/FiraCode-${type}.ttf \
     "https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-${type}.ttf?raw=true";
 done
 
+# Old School PC fonts
+wget http://int10h.org/oldschool-pc-fonts/download/ultimate_oldschool_pc_font_pack_v1.0.zip
+unzip ultimate_oldschool_pc_font_pack_v1.0.zip
+
+fc-cache -v
