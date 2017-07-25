382d2d3c7a3c167ce44930db57eb32444912fa4a
Gazaunga <Gazaunga@gmail.com>
2017-07-25 16:17:02 -0400

    Create webdev.sh

---
+++ webdev.sh
@@ -0,0 +1,24 @@
+# Nodejs and NVM
+curl https://raw.githubusercontent.com/creationix/nvm/v0.16.1/install.sh | sh
+source ~/.profile
+sudo nvm install 4.2.3
+sudo nvm use 4.2.3
+node -v
+
+# Forever to run nodejs scripts forever
+sudo npm install forever -g
+
+# Grunt - an automated task runner
+sudo npm install -g grunt-cli
+
+# Bower - a dependency manager
+sudo npm install -g bower
+
+# Yeoman - for generators
+sudo npm install -g yo 
+
+# Gulp - an automated task runner
+sudo npm install -g gulp-cli
+
+# Angular FullStack - My favorite MEAN boilerplate (MEAN = MongoDB, Express, Angularjs, Nodejs)
+sudo npm install -g generator-angular-fullstack
